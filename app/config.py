ca4e6e3d4f5ab97c323fc74b5df8ab189fd92da4
rafkix <rafkixuz@gmail.com>
2025-12-07 17:31:04 +0000

    edit

--- app/config.py
+++ app/config.py
@@ -1,30 +1,23 @@
-import os
+# import os
 from dotenv import load_dotenv
 
 # .env faylni yuklash
 load_dotenv()
 
 # 🗃 Database
-DB_PATH = os.getenv("DB_PATH", "walle.db")
+DB_PATH = "walle.db"
 
 # 🔐 JWT
-SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRE_MINUTES", 60))
+SECRET_KEY = "rafkix"
+ALGORITHM = "HS256"
+TOKEN_EXPIRE_MINUTES = 120
 
 # 🌐 OAuth (external logins)
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+GOOGLE_CLIENT_ID="260764636744-0iukf6ukmljlatu0ulfrcl96s87e13l5.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET="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"
+TELEGRAM_BOT_TOKEN="8453663132:AAGPfG-yhzZWKFQ_Sg2oMY7RpuINC6Mr3YY"
 
-ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+ALLOWED_ORIGINS = ["*", "enwis.uz", "www.enwis.uz", "api.enwis.uz", "app.enwis.uz"]
 
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY", "9324f246c7ef4e887bff6bcdfd4815f91969fed11e4742b2b47bf0d0b1eb6a02")
-ELEVENLABS_VOICE_ID = os.getenv("ELEVENLABS_VOICE_ID", "TxGEqnHWrfWFTfGW9XjX")
-
-
-# (Optional) debug uchun
-if not GOOGLE_CLIENT_ID:
-    print("⚠️ GOOGLE_CLIENT_ID not set")
-if not TELEGRAM_BOT_TOKEN:
-    print("⚠️ TELEGRAM_BOT_TOKEN not set")
+ELEVENLABS_API_KEY = "9324f246c7ef4e887bff6bcdfd4815f91969fed11e4742b2b47bf0d0b1eb6a02"
+ELEVENLABS_VOICE_ID = "TxGEqnHWrfWFTfGW9XjX"
